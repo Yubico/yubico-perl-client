502efdde0ed37c8e1cfd8280211248df07f32cfe
Dain Nilsson <dain@yubico.com>
2013-02-21 16:29:59 +0100

    Added release publishing to make-release.sh

--- make-release.sh
+++ make-release.sh
@@ -10,6 +10,8 @@ fi
 PACKAGE="AnyEvent-Yubico"
 VERSION=$(cat lib/AnyEvent/Yubico.pm | grep "our \$VERSION" | sed 's/^[^0-9]*\([0-9]\{1,\}\(\.[0-9]\{1,\}\)*\)[^0-9]*$/\1/g')
 
+echo "Releasing $PACKAGE version $VERSION...";
+
 if [ "$(git tag | grep $PACKAGE-$VERSION)" == "$PACKAGE-$VERSION" ]; then
     echo "git tag '$PACKAGE-$VERSION' already exists! Did you forget to update the version number?"
     exit 1
@@ -24,22 +26,30 @@ if [ ! "$(cat Changes | grep -e "$PATTERN")" ]; then
 fi
 
 do_test="true"
-do_sign="true"
+do_publish="true"
 
 if [ "x$1" == "x--no-test" ]; then
+    echo "Skip tests"
     do_test="false"
+    shift
+fi
+
+if [ "x$1" == "x--no-publish" ]; then
+    echo "Do not publish release"
+    do_publish="false"
     shift
 fi
 
 KEYID=$1
 if [ "x$KEYID" == "x" ]; then
-    echo "Syntax: $0 [--no-test] <KEYID>";
+    echo "Syntax: $0 [--no-test] [--no-publish] <KEYID>";
     exit 1;
 fi
 
 perl Makefile.PL
 
 if [ "x$do_test" != "xfalse" ]; then
+    echo "Running tests..."
     make test
 fi
 
@@ -50,5 +60,26 @@ make distclean
 gpg --detach-sign --default-key $KEYID $PACKAGE-$VERSION.tar.gz
 gpg --verify $PACKAGE-$VERSION.tar.gz.sig
 
-git tag -u $KEYID -m $VERSION $PACKAGE-$VERSION
+if [ "x$do_publish" != "xfalse" ]; then
+    echo "Publishing artifacts..."
+    git tag -u $KEYID -m $VERSION $PACKAGE-$VERSION
+    git push --tags
 
+    #Update gh-pages
+    git checkout gh-pages
+    mv $PACKAGE-$VERSION.tar.gz releases/
+    mv $PACKAGE-$VERSION.tar.gz.sig releases/
+
+    git add releases/$PACKAGE-$VERSION.tar.gz
+    git add releases/$PACKAGE-$VERSION.tar.gz.sig
+
+    versions=$(ls -1v releases/*.tar.gz | awk -F\- '{print $3}' | sed 's/\.tar\.gz//' | paste -sd ',' - | sed 's/,/, /g' | sed 's/\([0-9.]\{1,\}\)/"\1"/g')
+    sed -i -e "2s/\[.*\]/[$versions]/" releases.html
+    git add releases.html
+
+    git commit -m "Added tarball for release $VERSION"
+    git push
+    git checkout master
+fi
+
+echo "Success!"
